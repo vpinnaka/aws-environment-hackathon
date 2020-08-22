21b294e9f03983e6d040f852a90e8503e69e78e3
qpeng9 <qycs@gatech.edu>
2020-08-21 17:32:05 -0700

    convert to py

---
+++ python_code/1_train_predictor.py
@@ -0,0 +1,197 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 21 17:09:05 2020
+
+@author: qiaoyunpeng
+"""
+
+import matplotlib.pylab as plt
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+from pathlib import Path
+import time
+import os
+
+
+# Load Data
+dataset = pd.read_csv( index_col = 0,  parse_dates = True, infer_datetime_format = True, 
+                         filepath_or_buffer = 'data/2018-01-01__2019-01-01__NConservatory__allMerged.csv')
+
+dataset.index = pd.to_datetime(dataset.index, utc=True).tz_convert('America/Los_Angeles')
+
+# test data
+import joblib
+test_data = joblib.load('DMC20_Data/test_dataset_2019.numpy')
+
+orderedSensorList = ( 'co2_1','co2_2', 'co2_3', 'co2_4',                        
+                      'temp_1', 'temp_2', 'temp_3', 'temp_4',                     
+                      'dew_1','dew_2', 'dew_3', 'dew_4',
+                      'relH_1', 'relH_2', 'relH_3', 'relH_4',
+                      'externTemp_1', 
+                      'externHumid_1', 
+                      'externSunrise_1',                      
+                      'externCondition_1' )  
+
+orderedDataset = dataset.reindex( index = dataset.index, columns = orderedSensorList ) 
+
+
+# separate weekdays
+dayIndexDF = pd.Series(index = orderedDataset.index, 
+                       data = np.round(orderedDataset.index.dayofweek/6, decimals=2), 
+                       name='dayIndex')
+hourIndexDF = pd.Series(index = orderedDataset.index, 
+                       data = np.round(orderedDataset.index.hour/24, decimals=2), 
+                       name='hourIndex')
+
+orderedDatasetTimeReference = pd.concat([orderedDataset, hourIndexDF, dayIndexDF], axis=1)
+
+saturdayVal = np.round(5/6,decimals=2)
+sundayVal = np.round(6/6,decimals=2)
+
+weekdayData = orderedDatasetTimeReference[ ( dayIndexDF != saturdayVal) &( dayIndexDF != sundayVal) ]
+weekendData = orderedDatasetTimeReference[ ( dayIndexDF == saturdayVal) | (dayIndexDF == sundayVal) ]
+
+
+# Rescale/normalize data
+continuousData = weekdayData.values[:, 0:17] # first 17 sensors are continuous valued
+categoricalData = weekdayData.values[:, 17:]
+
+standardScaler = StandardScaler()
+standardScaler.fit( continuousData )
+
+minMaxScaler = MinMaxScaler()
+minMaxScaler.fit( categoricalData )
+
+scaledContinuousData = standardScaler.transform(continuousData)
+scaledCategoricalData = minMaxScaler.transform(categoricalData)
+
+weekdayData_scaled =  pd.DataFrame( index = weekdayData.index,
+                                    data = np.hstack( (scaledContinuousData, scaledCategoricalData)),
+                                    columns = weekdayData.columns)
+
+
+def build_train_data ( inputDataframe, outputFilename, advanceTimedeltaStr = '15 min' ):
+
+    npFilename = outputFilename + '.npy'
+    npFilenameSamplebounds = outputFilename + '_sampleBounds' + '.npy'
+    rebuildFlag = True # flips if file exists in cache and user agrees to restore
+
+    windowCounter = correctLenWindows = fixedWindows = 1
+    trimNeededWindows = extensionNeededWindows = 1
+    sumExtensionAmount = sumTrimAmount = 0
+    
+    def extract_one_day( startTime, endTime, idealIndex, nExtensionAttempts=10 ):        
+        nonlocal correctLenWindows, trimNeededWindows, extensionNeededWindows, \
+                sumExtensionAmount, sumTrimAmount, fixedWindows
+
+        oneDay = inputDataframe[ (inputDataframe.index >= startTime) \
+                                         & (inputDataframe.index < endTime)]
+        
+        if len(oneDay) == len(idealIndex):
+            correctLenWindows += 1
+
+        # available data in day range is too long -- trimming required
+        if len(oneDay) > len(idealIndex):
+            trimNeededWindows += 1
+            sumTrimAmount += len(oneDay) - len(idealIndex)
+            oneDay = oneDay.iloc[0:len(idealIndex)]
+        
+        # available data in day range is of insufficient length -- extension required 
+        if len(oneDay) < len(idealIndex):
+            extensionNeededWindows += 1
+            sumExtensionAmount += len(idealIndex) - len(oneDay)
+            
+            oneDay = None
+            # attempt to extend one sample at a time up to nExtensionAttempts
+            for iExtensionAttempt in range( nExtensionAttempts ):
+                endTime += pd.Timedelta( advanceTimedeltaStr)
+                extendedDay = inputDataframe[ (inputDataframe.index >= startTime) \
+                                                 & (inputDataframe.index < endTime)]
+                # sucessfully extened data slice
+                if len( extendedDay) == len( idealIndex ):
+                    fixedWindows += 1
+                    oneDay = extendedDay
+                    break
+        
+        # only happens when available data requires extension beyond nExtensionAttempts
+        if oneDay is None:
+            return None, None, None
+        
+        assert ( len(oneDay) == len(idealIndex) )
+        return oneDay.values.reshape(1,-1, order = 'F'), oneDay.index[0], oneDay.index[-1]
+    
+    
+    # check cache
+    if Path(npFilename).is_file() and Path(npFilenameSamplebounds).is_file():
+        print('created on: {} \n\t size: {} MB'.format( time.ctime(os.path.getctime(npFilename)), 
+                                                        Path(npFilename).stat().st_size / 1e6 ))
+        if 'y' == input('load from cache? (y/n): '):
+            npTrainMatrix = np.load(npFilename, allow_pickle=True) # load from cache
+            sampleIndexBounds = np.load(npFilenameSamplebounds, allow_pickle=True)
+            rebuildFlag = False
+    
+    if rebuildFlag:
+        npTrainMatrix = None
+        sampleIndexBounds = None
+        npTestMatrix = None
+
+        startTime = inputDataframe.index[0]
+        while(1):
+            endTime = startTime + pd.Timedelta('1 day');
+            
+            # terminate window extraction once end of dataset is reached
+            if startTime > inputDataframe.index[-1] or endTime > inputDataframe.index[-1]:
+                break
+            
+            # generate an ideal index [ 96 x 15 min samples from current startTime ]
+            idealIndex = pd.date_range(start=startTime, end=endTime, freq='15T', closed='left' );
+            
+            # ensure that current window is a weekday
+            if (idealIndex.dayofweek < 5).all():
+                
+                # gather data from sensor streams going one day forward
+                dataWindow, windowStartIndex, windowEndIndex = \
+                    extract_one_day ( startTime, endTime, idealIndex)
+                
+                # dataWindow is none only when more than ~10 samples are missing                
+                if dataWindow is not None:                
+                    windowCounter += 1
+                    
+                    # first iteration is a direct assignment, all others require appending 
+                    if npTrainMatrix is None:
+                        npTrainMatrix = dataWindow;
+                        sampleIndexBounds = np.array([windowStartIndex, windowEndIndex]);
+                    else:
+                        assert( dataWindow.shape[1] == npTrainMatrix.shape[1] ) # import ipdb; ipdb.set_trace()
+                        npTrainMatrix = np.append( npTrainMatrix, dataWindow, axis = 0 );
+                        sampleIndexBounds = np.append( sampleIndexBounds, 
+                                                       np.array([windowStartIndex, windowEndIndex]) , axis = 0);
+                        
+            else:
+                pass # skipping -- day segment includes a piece of the weekend
+            
+            avgTrim = np.round(sumTrimAmount/trimNeededWindows, decimals=2);
+            avgExtend = np.round(sumExtensionAmount/extensionNeededWindows, decimals=2);
+            
+            # advance to next sample
+            startTime = startTime + pd.Timedelta(advanceTimedeltaStr);
+
+        print('nWindows {} - nCorrect {}; nTrimNeeded {} | avg.trim {}; nExtendNeeded {} | avg.ext {} | fixedViaExtension {}'.format(windowCounter, correctLenWindows, 
+                       trimNeededWindows, avgTrim, extensionNeededWindows, avgExtend, fixedWindows), end='\r')
+
+        print('\nwriting to cache')
+        
+        # save to cache
+        sampleIndexBounds = sampleIndexBounds.reshape(npTrainMatrix.shape[0], -1)
+        np.save(npFilename, npTrainMatrix)
+        np.save(npFilenameSamplebounds, sampleIndexBounds)
+
+    if sampleIndexBounds.shape[0] != npTrainMatrix.shape[0]:
+        sampleIndexBounds = sampleIndexBounds.reshape(npTrainMatrix.shape[0], -1)
+        
+    return npTrainMatrix, sampleIndexBounds
+
+outputFilename = '../data/2018-01-01__2019-01-01__NConservatory_npWeekdayAllOrderedSensorsTimeRef'
+npTrainMatrix, sampleIndexBounds = build_train_data ( weekdayData_scaled, outputFilename )
